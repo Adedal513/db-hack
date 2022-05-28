ef4bcf2fb72286aa5adbe0894829e8f1422e45e1
Nikolay Moroz <68197237+Adedal513@users.noreply.github.com>
2022-05-28 08:23:36 +0300

    Fixed fix_marks()
    - Fixed method
    - Updated README

--- scripts.py
+++ scripts.py
@@ -18,34 +18,31 @@ def get_student_by_name(student_name: str):
 
 def create_commendation(student: str, subject_title: str) -> bool:
     target_student = get_student_by_name(student)
-
-    if target_student is None:
-        return False
-
-    subjects_available = (subject.title for subject in Subject.objects.all())
     possible_commendations = ['Хвалю!', 'Молодец', 'Так держать!', 'Ты сегодня прыгнул выше головы!',
                               'Здорово!', 'Гораздо лучше, чем я ожидал!']
 
-    if subject_title in subjects_available:
-        student_target_lessons = Lesson.objects.filter(
-            year_of_study=target_student.year_of_study,
-            group_letter=target_student.group_letter,
-            subject__title=subject_title
+    if not target_student:
+        return False
+
+    student_target_lessons = Lesson.objects.filter(
+        year_of_study=target_student.year_of_study,
+        group_letter=target_student.group_letter,
+        subject__title=subject_title
+    )
+
+    if student_target_lessons.count():
+        commendation_lesson = random.choice(student_target_lessons)
+        commendation_text = random.choice(possible_commendations)
+
+        Commendation.objects.create(
+            text=commendation_text,
+            created=commendation_lesson.date,
+            schoolkid=target_student,
+            subject=commendation_lesson.subject,
+            teacher=commendation_lesson.teacher
         )
 
-        if student_target_lessons.count():
-            commendation_lesson = random.choice(student_target_lessons)
-            commendation_text = random.choice(possible_commendations)
-
-            Commendation.objects.create(
-                text=commendation_text,
-                created=commendation_lesson.date,
-                schoolkid=target_student,
-                subject=commendation_lesson.subject,
-                teacher=commendation_lesson.teacher
-            )
-
-            return True
+        return True
 
     return False
 
@@ -53,10 +50,10 @@ def create_commendation(student: str, subject_title: str) -> bool:
 def remove_chastisements(student: str):
     target_student = get_student_by_name(student)
 
-    if target_student is None:
+    if not target_student:
         return False
 
-    student_chastisements = Chastisement.objects.filter(schoolkid=student.id)
+    student_chastisements = Chastisement.objects.filter(schoolkid=target_student.id)
     student_chastisements.delete()
 
     return True
@@ -65,10 +62,12 @@ def remove_chastisements(student: str):
 def fix_marks(student: str, limit=4):
     target_student = get_student_by_name(student)
 
-    if target_student is None:
+    if not target_student:
         return False
 
+    good_mark = random.choice(range(limit, 6))
+
     bad_marks = Mark.objects.filter(schoolkid=target_student.id, points__lt=limit)
-    bad_marks.delete()
+    bad_marks.update(points=good_mark)
 
     return True
